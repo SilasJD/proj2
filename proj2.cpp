d71f2cd86db6907ecdabe612e4a97d0994e1a265
SilasJD <44007087+SilasJD@users.noreply.github.com>
2018-10-09 17:18:19 -0400

    Initial commit

---
+++ proj2.cpp
@@ -0,0 +1,19 @@
+/* Name: Silas Dunigan
+   Title: proj2.cpp
+   Date: 18 October 2017
+   Description: Starts a new game. */
+
+#include "Troop.h"
+#include "Game.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main () {
+  Game newGame;
+
+  return 0;
+}
